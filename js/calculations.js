cab7a65b3be495c73f5adf145172d1dd43ef2b48
ThomasF85 <thomas.foeldi@gmail.com>
2022-09-14 19:27:54 +0200

    add test for cubicVolume

--- js/calculations.js
+++ js/calculations.js
@@ -1,3 +1,8 @@
 function cubicVolume(length, width, depth) {
+  if (length < 0 || width < 0 || depth < 0) {
+    return 0;
+  }
   return length * width * depth;
 }
+
+export { cubicVolume };
---
+++ js/calculations.test.js
@@ -0,0 +1,18 @@
+import { cubicVolume } from "./calculations.js";
+
+const testData = [
+  [2, 3, 4, 24],
+  [4, 3, 4, 48],
+  [5, 5, 5, 125],
+  [1, 0, 3, 0],
+  [1, -4, 3, 0],
+  [-1, 2, 5, 0],
+];
+
+test.each(testData)(
+  "returns correct teenager status",
+  (length, width, depth, expectedVolume) => {
+    const volume = cubicVolume(length, width, depth);
+    expect(volume).toBe(expectedVolume);
+  }
+);
